99f681cb73966a61ca92e1c6dda0576c5f7b16d4
Exequiel Ignacio SOsa <75383904+exequielsosa@users.noreply.github.com>
2021-01-12 12:41:06 -0300

    Modal con componentes

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+import './App.css';
+import Modal from './Modal';
+
+
+function App() {
+  const [active, setActive] = useState(false);
+
+  const toggle = () => {
+    setActive(!active);
+  }
+  return (
+    <div className="App">
+      <button style={{
+        position: 'absolute',
+        top: '50%',
+        padding: 10,
+      }} onClick={toggle}>Open Modal</button>
+    <Modal active={active} toggle={toggle}>
+    <h1>Modal Works!</h1>
+
+
+    </Modal>
+
+
+    </div>
+  );
+}
+
+export default App;
